66458777c5cd879eca145a09a592adf07bfe5baf
James Jackson <superjax08@gmail.com>
2017-10-09 02:08:55 -0600

    working Tx, still need to test Rx

--- examples/uart_printf/main.cpp
+++ examples/uart_printf/main.cpp
@@ -45,13 +45,10 @@ int main()
   uartPtr = &uart;
   uart.register_rx_callback(rx_callback);
 
-  uint8_t size = 30;
   while(1)
   {
-    uint8_t hello_string[size];
-    for (uint8_t i = 0; i < size; i++)
-      hello_string[i] = i;
-    uart.write(hello_string, size);
+    uint8_t hello_string[9] = "testing\n";
+    uart.write(hello_string, 8);
     delay(200);
   }
 }
