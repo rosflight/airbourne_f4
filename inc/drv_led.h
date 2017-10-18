9609c81a58cff6441b8cc0d7a88cac8da7098e08
James Jackson <superjax08@gmail.com>
2017-10-18 01:36:39 -0600

    fixed LEDs

--- inc/drv_led.h
+++ inc/drv_led.h
@@ -1,26 +1,21 @@
 #ifndef DRV_LED_H
 #define DRV_LED_H
 
-#include "system.h"
+#include "revo_f4.h"
+#include "gpio.h"
 
-class LED {
+class LED : public GPIO
+{
 public:
 
   /**
    * @param gpio_port  GPIO port to use (ie GPIOA, GPIOB, etc)
    * @param pin  The pin within this port to use (ie: GPIO_Pin_5)
    */
-  LED(GPIO_TypeDef *gpio_port, uint16_t pin);
-
-  void toggle();
+  void init(GPIO_TypeDef *gpio_port, uint16_t pin);
 
   void on();
-
   void off();
-
-private:
-  uint16_t pin_;
-  GPIO_TypeDef *port_;
 };
 
 #endif // DRV_LED_H
