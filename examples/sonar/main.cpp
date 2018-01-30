647783dd7936c2c02f9d803b4b1677086ca2e6e0
Trey Henrichsen <treyhenrichsen@gmail.com>
2018-01-30 14:16:33 -0700

    Update references to other files, remove led's

--- examples/sonar/main.cpp
+++ examples/sonar/main.cpp
@@ -1,10 +1,9 @@
 #include <string>
 #include "revo_f4.h"
 
-#include "drv_spi.h"
-#include "drv_i2c.h"
+#include "spi.h"
+#include "i2c.h"
 #include "mpu6000.h"
-#include "drv_led.h"
 #include "drv_mb1242.h"
 
 
@@ -13,13 +12,9 @@ int main() {
 
   systemInit();
 
-  LED warn(LED1_GPIO, LED1_PIN);
-  LED info(LED2_GPIO, LED2_PIN);
 
-  delay(500);
-
-  info.on();
-  I2C i2c1(I2C2);
+  I2C i2c1;
+  i2c1.init(&i2c_config[EXTERNAL_I2C]);
   I2CSonar sonar(i2c1);
   
 
