2abd8dbfa82ab71a947ac259750c901c2ddf3a05
BillThePlatypus <treyhenrichsen@gmail.com>
2019-01-16 12:01:22 -0700

    Restructure for better cross-board compatibility

--- include/backup_sram.h
+++ include/backup_sram.h
@@ -2,28 +2,12 @@
 #define BACKUP_SRAM_H
 
 #include <stdint.h>
-struct debug_info_t{
-    uint32_t r0;
-    uint32_t r1;
-    uint32_t r2;
-    uint32_t r3;
-    uint32_t r12;
-    uint32_t lr;
-    uint32_t pc;
-    uint32_t psr;
-};
-struct backup_data_t{
-    uint32_t error_code;
-    debug_info_t debug_info;
-    uint32_t reset_count;
-    uint32_t arm_status;
-    //TODO add state manager info
-    uint32_t checksum; //With the current implementation of the checksum, this must go last
-};
+#include "board.h"
+
 void backup_sram_init();
-void backup_sram_write(const backup_data_t&);
-backup_data_t backup_sram_read();
-bool check_backup_checksum(const backup_data_t&);
-uint32_t generate_backup_checksum(const backup_data_t&);
+void backup_sram_write(const rosflight_firmware::backup_data_t&);
+rosflight_firmware::backup_data_t backup_sram_read();
+bool check_backup_checksum(const rosflight_firmware::backup_data_t&);
+uint32_t generate_backup_checksum(const rosflight_firmware::backup_data_t&);
 void test_backup_sram();
 #endif // BACKUP_SRAM_H
--- src/backup_sram.cpp
+++ src/backup_sram.cpp
@@ -5,7 +5,7 @@
 
 void backup_sram_init()
 {
-    static_assert(sizeof(backup_data_t)%4==0,"Error with debug_info_t: Size is not multiple of 32 bits");
+    static_assert(sizeof(rosflight_firmware::backup_data_t)%4==0,"Error with debug_info_t: Size is not multiple of 32 bits");
 #pragma GCC diagnostic push //Ignore old style cast from included library
 #pragma GCC diagnostic ignored "-Wold-style-cast"
     RCC_APB1PeriphResetCmd(RCC_APB1Periph_PWR, DISABLE);
@@ -14,40 +14,40 @@ void backup_sram_init()
 #pragma GCC diagnostic pop
     PWR_BackupRegulatorCmd(ENABLE);
 }
-void backup_sram_write(const backup_data_t& data)
+void backup_sram_write(const rosflight_firmware::backup_data_t& data)
 {
     PWR_BackupAccessCmd(ENABLE);
 #pragma GCC diagnostic push //Ignore old style cast from included library
 #pragma GCC diagnostic ignored "-Wold-style-cast"
-    std::memcpy(reinterpret_cast<void*>(BKPSRAM_BASE),&data,sizeof(backup_data_t));
+    std::memcpy(reinterpret_cast<void*>(BKPSRAM_BASE),&data,sizeof(rosflight_firmware::backup_data_t));
 #pragma GCC diagnostic pop
     PWR_BackupAccessCmd(DISABLE);
 }
-backup_data_t backup_sram_read()
+rosflight_firmware::backup_data_t backup_sram_read()
 {
 #pragma GCC diagnostic push //Ignore old style cast from included library
 #pragma GCC diagnostic ignored "-Wold-style-cast"
-    backup_data_t data = *(reinterpret_cast<backup_data_t*>(BKPSRAM_BASE));
+    static rosflight_firmware::backup_data_t data = *(reinterpret_cast<rosflight_firmware::backup_data_t*>(BKPSRAM_BASE));
 #pragma GCC diagnostic pop
     return data;
 }
-bool check_backup_checksum(const backup_data_t& data)
+bool check_backup_checksum(const rosflight_firmware::backup_data_t& data)
 {
     return (data.checksum==generate_backup_checksum(data));
 }
-uint32_t generate_backup_checksum(const backup_data_t& data)
+uint32_t generate_backup_checksum(const rosflight_firmware::backup_data_t& data)
 {
     const uint32_t* pointer = reinterpret_cast<const uint32_t*>(&data);
     uint32_t checksum = 0;
-    for(uint8_t i=0;i<(sizeof (backup_data_t)/4)-1;i++)
+    for(uint8_t i=0;i<(sizeof (rosflight_firmware::backup_data_t)/4)-1;i++)
         checksum^=*(pointer+i);
     return checksum;
 }
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers" //Remove this, too
 void test_backup_sram(){
   backup_sram_init();
-  backup_data_t read_data = backup_sram_read();
-  backup_data_t write_data = {};
+  rosflight_firmware::backup_data_t read_data = backup_sram_read();
+  rosflight_firmware::backup_data_t write_data = {};
   write_data.error_code = 0xabcde;
   if(check_backup_checksum(read_data))
   {
