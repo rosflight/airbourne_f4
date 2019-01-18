35fbbcd27ad8be2dcb21e4b831b28602ab07e398
BillThePlatypus <treyhenrichsen@gmail.com>
2019-01-18 09:18:20 -0700

    Attempt to convert backup_sram to singleton object

--- include/backup_sram.h
+++ include/backup_sram.h
@@ -4,10 +4,31 @@
 #include <stdint.h>
 #include "board.h"
 
-void backup_sram_init();
+#ifdef __cplusplus
+class backup_sram {
+public:
+    void init();
+    void write(const rosflight_firmware::backup_data_t&);
+    rosflight_firmware::backup_data_t read();
+    bool has_data();
+    static uint32_t generate_backup_checksum(const rosflight_firmware::backup_data_t&);
+    static backup_sram& get_instance() {return backup_sram::instance;}
+private:
+    backup_sram();
+    rosflight_firmware::backup_data_t do_first_read();
+    rosflight_firmware::backup_data_t data;
+    bool valid_data=false;
+    static bool is_valid(const rosflight_firmware::backup_data_t&);
+    static backup_sram instance;
+};
+
+extern "C" {
+#endif // __cplusplus
+
 void backup_sram_write(const rosflight_firmware::backup_data_t&);
-rosflight_firmware::backup_data_t backup_sram_read();
-bool check_backup_checksum(const rosflight_firmware::backup_data_t&);
 uint32_t generate_backup_checksum(const rosflight_firmware::backup_data_t&);
-void test_backup_sram();
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
 #endif // BACKUP_SRAM_H
