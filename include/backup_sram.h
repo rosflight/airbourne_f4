c17832fc741923dd294333f038496703750ebabd
BillThePlatypus <treyhenrichsen@gmail.com>
2019-01-18 09:23:28 -0700

    Revert "Attempt to convert backup_sram to singleton object"
    This reverts commit 35fbbcd27ad8be2dcb21e4b831b28602ab07e398.

--- include/backup_sram.h
+++ include/backup_sram.h
@@ -4,31 +4,10 @@
 #include <stdint.h>
 #include "board.h"
 
-#ifdef __cplusplus
-class backup_sram {
-public:
-    void init();
-    void write(const rosflight_firmware::backup_data_t&);
-    rosflight_firmware::backup_data_t read();
-    bool has_data();
-    static uint32_t generate_backup_checksum(const rosflight_firmware::backup_data_t&);
-    static backup_sram& get_instance() {return backup_sram::instance;}
-private:
-    backup_sram();
-    rosflight_firmware::backup_data_t do_first_read();
-    rosflight_firmware::backup_data_t data;
-    bool valid_data=false;
-    static bool is_valid(const rosflight_firmware::backup_data_t&);
-    static backup_sram instance;
-};
-
-extern "C" {
-#endif // __cplusplus
-
+void backup_sram_init();
 void backup_sram_write(const rosflight_firmware::backup_data_t&);
+rosflight_firmware::backup_data_t backup_sram_read();
+bool check_backup_checksum(const rosflight_firmware::backup_data_t&);
 uint32_t generate_backup_checksum(const rosflight_firmware::backup_data_t&);
-
-#ifdef __cplusplus
-}
-#endif // __cplusplus
+void test_backup_sram();
 #endif // BACKUP_SRAM_H
--- src/backup_sram.cpp
+++ src/backup_sram.cpp
@@ -3,7 +3,7 @@
 #include "stm32f4xx_rcc.h"
 #include <cstring>
 
-void backup_sram::init()
+void backup_sram_init()
 {
     static_assert(sizeof(rosflight_firmware::backup_data_t)%4==0,"Error with debug_info_t: Size is not multiple of 32 bits");
 #pragma GCC diagnostic push //Ignore old style cast from included library
@@ -13,43 +13,29 @@ void backup_sram::init()
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_BKPSRAM, ENABLE);
 #pragma GCC diagnostic pop
     PWR_BackupRegulatorCmd(ENABLE);
-
 }
-void backup_sram::write(const rosflight_firmware::backup_data_t& write_data)
+void backup_sram_write(const rosflight_firmware::backup_data_t& data)
 {
     PWR_BackupAccessCmd(ENABLE);
 #pragma GCC diagnostic push //Ignore old style cast from included library
 #pragma GCC diagnostic ignored "-Wold-style-cast"
-    std::memcpy(reinterpret_cast<void*>(BKPSRAM_BASE),&write_data,sizeof(rosflight_firmware::backup_data_t));
+    std::memcpy(reinterpret_cast<void*>(BKPSRAM_BASE),&data,sizeof(rosflight_firmware::backup_data_t));
 #pragma GCC diagnostic pop
     PWR_BackupAccessCmd(DISABLE);
 }
-rosflight_firmware::backup_data_t backup_sram::do_first_read()
+rosflight_firmware::backup_data_t backup_sram_read()
 {
 #pragma GCC diagnostic push //Ignore old style cast from included library
 #pragma GCC diagnostic ignored "-Wold-style-cast"
-    static rosflight_firmware::backup_data_t read_data = *(reinterpret_cast<rosflight_firmware::backup_data_t*>(BKPSRAM_BASE));
+    static rosflight_firmware::backup_data_t data = *(reinterpret_cast<rosflight_firmware::backup_data_t*>(BKPSRAM_BASE));
 #pragma GCC diagnostic pop
-#pragma GCC diagnostic push //Ignore old style cast from included library
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
-    if(!is_valid(read_data))
-    {
-        read_data={0};
-        this->valid_data=false;
-    }
-    else
-        this->valid_data=true;
-#pragma GCC diagnostic pop
-    return read_data;
+    return data;
 }
-bool backup_sram::has_data() {
-   return this->valid_data;
-}
-bool backup_sram::is_valid(const rosflight_firmware::backup_data_t& data)
+bool check_backup_checksum(const rosflight_firmware::backup_data_t& data)
 {
     return (data.checksum==generate_backup_checksum(data));
 }
-uint32_t backup_sram::generate_backup_checksum(const rosflight_firmware::backup_data_t& data)
+uint32_t generate_backup_checksum(const rosflight_firmware::backup_data_t& data)
 {
     const uint32_t* pointer = reinterpret_cast<const uint32_t*>(&data);
     uint32_t checksum = 0;
@@ -57,16 +43,8 @@ uint32_t backup_sram::generate_backup_checksum(const rosflight_firmware::backup_
         checksum^=*(pointer+i);
     return checksum;
 }
-uint32_t generate_backup_checksum(const rosflight_firmware::backup_data_t& data)
-{
-    return backup_sram::generate_backup_checksum(data);
-}
-void backup_sram_write(const rosflight_firmware::backup_data_t& data)
-{
-    backup_sram::get_instance().write(data);
-}
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers" //Remove this, too
-/*void test_backup_sram(){
+void test_backup_sram(){
   backup_sram_init();
   rosflight_firmware::backup_data_t read_data = backup_sram_read();
   rosflight_firmware::backup_data_t write_data = {};
@@ -76,4 +54,4 @@ void backup_sram_write(const rosflight_firmware::backup_data_t& data)
   }
   write_data.checksum=generate_backup_checksum(write_data);
   backup_sram_write(write_data);
-}*/
+}
