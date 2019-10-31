1575b09d92f1af942ce43449ac6a37528c8657f9
Daniel Koch <dpkoch@users.noreply.github.com>
2019-10-30 22:11:05 -0600

    Backup SRAM cleanup (#43)
    * Cleaning up backup SRAM implementation
    
    * Finished cleaning up backup SRAM implementation

--- examples/backup_sram/main.cpp
+++ examples/backup_sram/main.cpp
@@ -37,27 +37,64 @@
 #include "revo_f4.h"
 #include "backup_sram.h"
 #include "vcp.h"
+#include "printf.h"
 
-void restart(){
+struct BackupData
+{
+    static constexpr uint32_t CHECKSUM_START = 0xA5A5;
+
+    uint32_t reset_count;
+    uint32_t some_data;
+    uint32_t checksum;
+
+    BackupData()
+    {
+        memset(this, 0, sizeof(BackupData));
+    }
+
+    uint32_t compute_checksum()
+    {
+        uint32_t crc = CHECKSUM_START;
+        for (size_t offset = 0; offset <= sizeof(BackupData) - sizeof(checksum) - sizeof(crc); offset++)
+        {
+            crc ^= *reinterpret_cast<uint32_t*>(reinterpret_cast<uint8_t*>(this) + offset);
+        }
+        return crc;
+    }
+};
+
+void restart()
+{
     NVIC_SystemReset();
 }
-int main() {
-	systemInit();
-    backup_sram_init();
+
+int main()
+{
+    systemInit();
     VCP vcp;
     vcp.init();
     backup_sram_init();
-    BackupData read_data = backup_sram_read();
+
+    // check for and process backup data if it exists
+    BackupData read_data;
+    backup_sram_read(&read_data, sizeof(read_data));
+
     uint32_t reset_count = 0;
-    if(check_backup_checksum(read_data))
+    if (read_data.checksum == read_data.compute_checksum())
         reset_count = read_data.reset_count;
-    BackupData write_data={};
-    write_data.reset_count=++reset_count;
-    write_data.error_code=0xDEADBEEF;
-    write_data.checksum=generate_backup_checksum(write_data);
+
+    // clear the backup data so it doesn't get read on normal startup
+    backup_sram_clear(sizeof(BackupData));
+
+    // Pretend we get a hardfault some time in the future; write backup data and reset.
+    // Normally this would be done in the hardfault interrupt handler.
     delay(300);
-    backup_sram_write(write_data);
+
+    BackupData write_data;
+    write_data.reset_count = ++reset_count; // increment reset counter
+    write_data.some_data = 0xDEADBEEF;
+    write_data.checksum = write_data.compute_checksum();
+    backup_sram_write(&write_data, sizeof(write_data));
+
     restart();
 }
-
-
