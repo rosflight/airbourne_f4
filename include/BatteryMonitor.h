01cbbfa899c4cd589bb18ef7d15884dbaca3be1b
BillThePlatypus <treyhenrichsen@gmail.com>
2019-11-11 14:40:59 -0700

    Add simple BatterMonitor class

---
+++ include/BatteryMonitor.h
@@ -0,0 +1,24 @@
+#ifndef BATTERYMONITOR_H
+#define BATTERYMONITOR_H
+
+#include "analog_pin.h"
+
+class BatteryMonitor
+{
+public:
+  void init(AnalogPin *voltage_pin, double voltage_multiplier, AnalogPin *current_pin, double current_multiplier);
+  void init(AnalogPin *voltage_pin, double voltage_multiplier);
+  double read_voltage();
+  double read_current();
+  void set_voltage_multiplier(double multiplier);
+  void set_current_multiplier(double multiplier);
+  bool has_current_sense();
+private:
+  AnalogPin *voltage_pin_;
+  AnalogPin *current_pin_;
+  double voltage_multiplier_;
+  double current_multiplier_;
+  bool has_current_sense_;
+};
+
+#endif // BATTERYMONITOR_H
---
+++ src/BatteryMonitor.cpp
@@ -0,0 +1,39 @@
+#include "BatteryMonitor.h"
+
+
+void BatteryMonitor::init(AnalogPin *voltage_pin, double voltage_multiplier, AnalogPin *current_pin,
+                          double current_multiplier)
+{
+  this->voltage_pin_ = voltage_pin;
+  this->voltage_multiplier_ = voltage_multiplier;
+  this->current_pin_ = current_pin;
+  this->current_multiplier_ = current_multiplier;
+  this->has_current_sense_ = true;
+}
+void BatteryMonitor::init(AnalogPin *voltage_pin, double voltage_multiplier)
+{
+  this->voltage_pin_ = voltage_pin;
+  this->voltage_multiplier_ = voltage_multiplier;
+  this->current_pin_ = nullptr;
+  this->current_multiplier_ = 0;
+  this->has_current_sense_ = false;
+}
+double BatteryMonitor::read_voltage()
+{
+  return this->voltage_pin_->read() * this->voltage_multiplier_;
+}
+
+void BatteryMonitor::set_voltage_multiplier(double multiplier)
+{
+  this->voltage_multiplier_  = multiplier;
+}
+
+void BatteryMonitor::set_current_multiplier(double multiplier)
+{
+  this->current_multiplier_ = multiplier;
+}
+
+bool BatteryMonitor::has_current_sense()
+{
+  return this->has_current_sense_;
+}
