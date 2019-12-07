d14a2331531ae959fa72164549c841144725ff77
Trey Henrichsen <treyhenrichsen@gmail.com>
2019-12-07 16:00:23 -0700

    Add battery monitor struct, change battery monitor to use floats

--- include/battery_monitor.h
+++ include/battery_monitor.h
@@ -2,23 +2,23 @@
 #define BATTERYMONITOR_H
 
 #include "analog_pin.h"
+#include "system.h"
 
 class BatteryMonitor
 {
 public:
-  void init(AnalogPin *voltage_pin, double voltage_multiplier, AnalogPin *current_pin, double current_multiplier);
-  void init(AnalogPin *voltage_pin, double voltage_multiplier);
-  double read_voltage();
-  double read_current();
+  void init(const battery_monitor_hardware_struct_t &def, AnalogDigitalConverter *adc, float voltage_multiplier, float current_multiplier);
+  float read_voltage();
+  float read_current();
   void set_voltage_multiplier(double multiplier);
   void set_current_multiplier(double multiplier);
   bool has_voltage_sense();
   bool has_current_sense();
 private:
-  AnalogPin *voltage_pin_;
-  AnalogPin *current_pin_;
-  double voltage_multiplier_;
-  double current_multiplier_;
+  AnalogPin voltage_pin_;
+  AnalogPin current_pin_;
+  float voltage_multiplier_;
+  float current_multiplier_;
 };
 
 #endif // BATTERYMONITOR_H
